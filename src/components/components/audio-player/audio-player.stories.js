809e7bd6a53134e595922f535cc28deb64d900dc
Randy Oest <oest@amazingrando.com>
2022-05-04 17:33:54 -0400

    feat: add link to figma

--- src/components/components/audio-player/audio-player.stories.js
+++ src/components/components/audio-player/audio-player.stories.js
@@ -1,8 +1,16 @@
-import audioPlayer from './audio-player.twig';
+import template from './audio-player.twig';
 import audioData from './audio-player.yml';
+import figma from '../../../../.storybook/configma.json';
 
 export default {
   title: 'Components/Media/Audio Player',
 };
 
-export const AudioPlayer = () => audioPlayer(audioData);
+export const AudioPlayer = () => template(audioData);
+
+AudioPlayer.parameters = {
+  design: {
+    type: 'figma',
+    url: figma.url + figma.audioPlayer,
+  },
+};
