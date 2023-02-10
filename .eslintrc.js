9e85f8680870cc98556a305ad27ebe0cedf93205
Randy Oest <oest@amazingrando.com>
2023-02-10 14:17:00 -0500

    feat: add compound 1.10.6 components

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
   extends: ['@fourkitchens/eslint-config-and-other-formatting'],
+  ignorePatterns: [
+    'src/components/components/01-atoms/images/icons/svgxuse.min.js',
+  ],
 };
--- stylelint.config.js
+++ stylelint.config.js
@@ -2,4 +2,25 @@ module.exports = {
   extends: [
     '@fourkitchens/eslint-config-and-other-formatting/stylelint.config',
   ],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: [
+          'at-root',
+          'each',
+          'else',
+          'extend',
+          'forward',
+          'function',
+          'if',
+          'include',
+          'mixin',
+          'return',
+          'use',
+          'while',
+        ],
+      },
+    ],
+  },
 };
