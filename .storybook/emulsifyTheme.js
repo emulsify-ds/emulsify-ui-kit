3e5eac1479fdcba59434b30b978e30cdae303d6d
Randy Oest <oest@amazingrando.com>
2023-03-28 13:53:07 -0400

    chore: work in progress

--- .storybook/emulsifyTheme.js
+++ .storybook/emulsifyTheme.js
@@ -3,10 +3,38 @@
 import { create } from '@storybook/theming';
 
 export default create({
-  base: 'light',
+  base: 'dark',
+
+  // UI
+  appBg: '#00405B',
+  appContentBg: '#005F89',
+  appBorderColor: '#99D9F4',
+  appBorderRadius: 4,
+
+  // Typography
+  fontBase: '"Mona Sans", sans-serif',
+  fontCode: 'monospace',
+
+  // Text colors
+  textColor: 'white',
+  textInverseColor: 'rgba(255,255,255,0.9)',
+  textMutedColor: '#E6F5FC',
+
+  // Toolbar default and active colors
+  barTextColor: '#005F89',
+  barSelectedColor: '#8B1E7E',
+  barBg: '#CCECFA',
+
+  // Form colors
+  inputBg: 'red',
+  inputBorder: 'silver',
+  inputTextColor: '#00202E',
+  inputBorderRadius: 4,
+  
   // Branding
   brandTitle: 'Emulsify',
   brandUrl: 'https://emulsify.info',
   brandImage:
-    'https://raw.githubusercontent.com/emulsify-ds/emulsify-design-system/master/images/logo.png',
+    'https://randyoest.com/random/emulsify-logo-sb.svg',
+  brandTarget: '_self',
 });
