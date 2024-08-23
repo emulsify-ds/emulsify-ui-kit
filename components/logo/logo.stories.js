9a4108362f08b9af9c76c622f932923a6666e766
Callin Mullaney <57088-callinmullaney@users.noreply.drupalcode.org>
2024-08-23 10:49:10 -0500

    feat: restore src directory

--- components/logo/logo.stories.js
+++
@@ -1,8 +0,0 @@
-import logo from './logo.twig';
-
-/**
- * Storybook Definition.
- */
-export default { title: 'Components/Logo' };
-
-export const Logo = () => logo();
--- components/tables/tables.stories.js
+++
@@ -1,9 +0,0 @@
-import tableTwig from './tables.twig';
-import tableData from './tables.yml';
-
-/**
- * Storybook Definition.
- */
-export default { title: 'Components/Typography' };
-
-export const tables = () => tableTwig(tableData);
