e93b003fc859e7ff542ec09d596140515331fece
Callin Mullaney <57088-callinmullaney@users.noreply.drupalcode.org>
2024-09-12 13:03:24 -0500

    feat: rework icon loading and twig file

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,5 +1,8 @@
 import buttonTwig from './button.twig';
 
+// control data.
+import controlData from '../../util/controlData.yml';
+
 export default {
   title: 'Components/Button',
   decorators: [(story) => `${story()}`],
@@ -23,17 +26,229 @@ export default {
         Secondary: 'secondary',
       },
     },
+    componentTheme: {
+      type: 'select',
+      name: 'Component Theme',
+      control: {
+        type: 'select',
+        options: controlData.color_variant,
+      },
+    },
   },
   args: {
     buttonElement: 'button',
     buttonStyle: 'filled',
+    componentTheme: 'base',
   },
 };
 
-export const Button = ({ buttonElement, buttonStyle }) =>
-  buttonTwig({
-    button__element: buttonElement,
-    button__href: '#',
-    button__content: `${buttonStyle} Button`,
-    button__style: buttonStyle,
-  });
+export const Button = ({ buttonElement, buttonStyle, componentTheme }) => `
+  <div class="cl-container">
+    <h1>${buttonStyle}</h1>
+  </div>
+  <div class="cl-container cl-spaced-row">
+    ${buttonTwig({
+      button__element: buttonElement,
+      button__href: '#',
+      button__content: `${buttonStyle} Button`,
+      button__style: buttonStyle,
+    })}
+    ${buttonTwig({
+      button__element: buttonElement,
+      button__href: '#',
+      button__content: `${buttonStyle} Icon Right`,
+      button__style: buttonStyle,
+      button__icon: {
+        right: true,
+        name: 'arrowRight',
+        decorative: true,
+      },
+    })}
+    ${buttonTwig({
+      button__element: buttonElement,
+      button__href: '#',
+      button__content: `${buttonStyle} Icon Left`,
+      button__style: buttonStyle,
+      button__icon: {
+        left: true,
+        name: 'arrowLeft',
+        decorative: true,
+      },
+    })}
+    ${buttonTwig({
+      button__element: buttonElement,
+      button__href: '#',
+      button__style: buttonStyle,
+      button__icon_only: 'Main Menu',
+      button__icon: {
+        left: true,
+        name: 'menu',
+        decorative: true,
+      },
+    })}
+  </div>
+  <div class="cl-container cl-spaced-row" data-component-theme="${componentTheme} dark inverse">
+    ${buttonTwig({
+      button__element: buttonElement,
+      button__href: '#',
+      button__content: `${buttonStyle} Button`,
+      button__style: buttonStyle,
+    })}
+    ${buttonTwig({
+      button__element: buttonElement,
+      button__href: '#',
+      button__content: `${buttonStyle} Icon Right`,
+      button__style: buttonStyle,
+      button__icon: {
+        right: true,
+        name: 'arrowRight',
+        decorative: true,
+      },
+    })}
+    ${buttonTwig({
+      button__element: buttonElement,
+      button__href: '#',
+      button__content: `${buttonStyle} Icon Left`,
+      button__style: buttonStyle,
+      button__icon: {
+        left: true,
+        name: 'arrowLeft',
+        decorative: true,
+      },
+    })}
+    ${buttonTwig({
+      button__element: buttonElement,
+      button__href: '#',
+      button__style: buttonStyle,
+      button__icon_only: 'disabled',
+      button__icon: {
+        left: true,
+        name: 'menu',
+        decorative: true,
+      },
+    })}
+  </div>
+  <div class="cl-container cl-spaced-row" data-component-theme="${componentTheme} base inverse">
+    ${buttonTwig({
+      button__element: buttonElement,
+      button__href: '#',
+      button__content: `${buttonStyle} Button`,
+      button__style: buttonStyle,
+    })}
+    ${buttonTwig({
+      button__element: buttonElement,
+      button__href: '#',
+      button__content: `${buttonStyle} Icon Right`,
+      button__style: buttonStyle,
+      button__icon: {
+        right: true,
+        name: 'arrowRight',
+        decorative: true,
+      },
+    })}
+    ${buttonTwig({
+      button__element: buttonElement,
+      button__href: '#',
+      button__content: `${buttonStyle} Icon Left`,
+      button__style: buttonStyle,
+      button__icon: {
+        left: true,
+        name: 'arrowLeft',
+        decorative: true,
+      },
+    })}
+    ${buttonTwig({
+      button__element: buttonElement,
+      button__href: '#',
+      button__style: buttonStyle,
+      button__icon_only: 'Main Menu',
+      button__icon: {
+        left: true,
+        name: 'menu',
+        decorative: true,
+      },
+    })}
+  </div>
+  <div class="cl-container cl-spaced-row" data-component-theme="${componentTheme} light">
+    ${buttonTwig({
+      button__element: buttonElement,
+      button__href: '#',
+      button__content: `${buttonStyle} Button`,
+      button__style: buttonStyle,
+    })}
+    ${buttonTwig({
+      button__element: buttonElement,
+      button__href: '#',
+      button__content: `${buttonStyle} Icon Right`,
+      button__style: buttonStyle,
+      button__icon: {
+        right: true,
+        name: 'arrowRight',
+        decorative: true,
+      },
+    })}
+    ${buttonTwig({
+      button__element: buttonElement,
+      button__href: '#',
+      button__content: `${buttonStyle} Icon Left`,
+      button__style: buttonStyle,
+      button__icon: {
+        left: true,
+        name: 'arrowLeft',
+        decorative: true,
+      },
+    })}
+    ${buttonTwig({
+      button__element: buttonElement,
+      button__href: '#',
+      button__style: buttonStyle,
+      button__icon_only: 'Main Menu',
+      button__icon: {
+        left: true,
+        name: 'menu',
+        decorative: true,
+      },
+    })}
+  </div>
+  <div class="cl-container cl-spaced-row" data-component-theme="${componentTheme} bright">
+    ${buttonTwig({
+      button__element: buttonElement,
+      button__href: '#',
+      button__content: `${buttonStyle} Button`,
+      button__style: buttonStyle,
+    })}
+    ${buttonTwig({
+      button__element: buttonElement,
+      button__href: '#',
+      button__content: `${buttonStyle} Icon Right`,
+      button__style: buttonStyle,
+      button__icon: {
+        right: true,
+        name: 'arrowRight',
+        decorative: true,
+      },
+    })}
+    ${buttonTwig({
+      button__element: buttonElement,
+      button__href: '#',
+      button__content: `${buttonStyle} Icon Left`,
+      button__style: buttonStyle,
+      button__icon: {
+        left: true,
+        name: 'arrowLeft',
+        decorative: true,
+      },
+    })}
+    ${buttonTwig({
+      button__element: buttonElement,
+      button__href: '#',
+      button__style: buttonStyle,
+      button__icon_only: 'Main Menu',
+      button__icon: {
+        left: true,
+        name: 'menu',
+        decorative: true,
+      },
+    })}
+  </div>
+`;
