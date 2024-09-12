75e7c97b7559135bb9cfe85ce7192be1ab5c001c
Callin Mullaney <57088-callinmullaney@users.noreply.drupalcode.org>
2024-09-12 16:07:40 -0500

    fix: cl styles throwing errors now that they import

--- src/components/button/button.stories.js
+++ src/components/button/button.stories.js
@@ -1,7 +1,7 @@
 import buttonTwig from './button.twig';
 
 // control data.
-import controlData from '../../util/controlData.yml';
+// import controlData from '../../util/controlData.yml';
 
 export default {
   title: 'Components/Button',
@@ -23,21 +23,14 @@ export default {
       name: 'Button Style',
       options: {
         Primary: 'primary',
-        Secondary: 'secondary',
-      },
-    },
-    componentTheme: {
-      type: 'select',
-      name: 'Component Theme',
-      control: {
-        type: 'select',
-        options: controlData.color_variant,
+        Clean: 'clean',
+        Disabled: 'disabled',
       },
     },
   },
   args: {
     buttonElement: 'button',
-    buttonStyle: 'filled',
+    buttonStyle: 'primary',
     componentTheme: 'base',
   },
 };
@@ -46,7 +39,7 @@ export const Button = ({ buttonElement, buttonStyle, componentTheme }) => `
   <div class="cl-container">
     <h1>${buttonStyle}</h1>
   </div>
-  <div class="cl-container cl-spaced-row">
+  <div class="cl-container cl-spaced-row" data-component-theme="${componentTheme}">
     ${buttonTwig({
       button__element: buttonElement,
       button__href: '#',
@@ -87,130 +80,7 @@ export const Button = ({ buttonElement, buttonStyle, componentTheme }) => `
       },
     })}
   </div>
-  <div class="cl-container cl-spaced-row" data-component-theme="${componentTheme} dark inverse">
-    ${buttonTwig({
-      button__element: buttonElement,
-      button__href: '#',
-      button__content: `${buttonStyle} Button`,
-      button__style: buttonStyle,
-    })}
-    ${buttonTwig({
-      button__element: buttonElement,
-      button__href: '#',
-      button__content: `${buttonStyle} Icon Right`,
-      button__style: buttonStyle,
-      button__icon: {
-        right: true,
-        name: 'arrowRight',
-        decorative: true,
-      },
-    })}
-    ${buttonTwig({
-      button__element: buttonElement,
-      button__href: '#',
-      button__content: `${buttonStyle} Icon Left`,
-      button__style: buttonStyle,
-      button__icon: {
-        left: true,
-        name: 'arrowLeft',
-        decorative: true,
-      },
-    })}
-    ${buttonTwig({
-      button__element: buttonElement,
-      button__href: '#',
-      button__style: buttonStyle,
-      button__icon_only: 'disabled',
-      button__icon: {
-        left: true,
-        name: 'menu',
-        decorative: true,
-      },
-    })}
-  </div>
-  <div class="cl-container cl-spaced-row" data-component-theme="${componentTheme} base inverse">
-    ${buttonTwig({
-      button__element: buttonElement,
-      button__href: '#',
-      button__content: `${buttonStyle} Button`,
-      button__style: buttonStyle,
-    })}
-    ${buttonTwig({
-      button__element: buttonElement,
-      button__href: '#',
-      button__content: `${buttonStyle} Icon Right`,
-      button__style: buttonStyle,
-      button__icon: {
-        right: true,
-        name: 'arrowRight',
-        decorative: true,
-      },
-    })}
-    ${buttonTwig({
-      button__element: buttonElement,
-      button__href: '#',
-      button__content: `${buttonStyle} Icon Left`,
-      button__style: buttonStyle,
-      button__icon: {
-        left: true,
-        name: 'arrowLeft',
-        decorative: true,
-      },
-    })}
-    ${buttonTwig({
-      button__element: buttonElement,
-      button__href: '#',
-      button__style: buttonStyle,
-      button__icon_only: 'Main Menu',
-      button__icon: {
-        left: true,
-        name: 'menu',
-        decorative: true,
-      },
-    })}
-  </div>
-  <div class="cl-container cl-spaced-row" data-component-theme="${componentTheme} light">
-    ${buttonTwig({
-      button__element: buttonElement,
-      button__href: '#',
-      button__content: `${buttonStyle} Button`,
-      button__style: buttonStyle,
-    })}
-    ${buttonTwig({
-      button__element: buttonElement,
-      button__href: '#',
-      button__content: `${buttonStyle} Icon Right`,
-      button__style: buttonStyle,
-      button__icon: {
-        right: true,
-        name: 'arrowRight',
-        decorative: true,
-      },
-    })}
-    ${buttonTwig({
-      button__element: buttonElement,
-      button__href: '#',
-      button__content: `${buttonStyle} Icon Left`,
-      button__style: buttonStyle,
-      button__icon: {
-        left: true,
-        name: 'arrowLeft',
-        decorative: true,
-      },
-    })}
-    ${buttonTwig({
-      button__element: buttonElement,
-      button__href: '#',
-      button__style: buttonStyle,
-      button__icon_only: 'Main Menu',
-      button__icon: {
-        left: true,
-        name: 'menu',
-        decorative: true,
-      },
-    })}
-  </div>
-  <div class="cl-container cl-spaced-row" data-component-theme="${componentTheme} bright">
+  <div class="cl-container cl-spaced-row" data-component-theme="${componentTheme} inverse">
     ${buttonTwig({
       button__element: buttonElement,
       button__href: '#',
